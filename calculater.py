d51498b6cc01048b1c9249221ea980bb0baec090
Dionshin <bigjoyshin@gmail.com>
2026-03-05 14:52:35 +0900

    곱하기 기능

--- calculater.py
+++ calculater.py
@@ -8,9 +8,16 @@ def sub(a,b):
     c = a - b
     return c
 
+def mul(a,b):
+    c= a*b
+    return c
+
+
 
 num1,num2 = map(int,input().split())
 add = add(num1,num2)
 sub = sub(num1,num2)
+mul = mul(num1, num2)
 print(add)
 print(sub)
+print(mul)
