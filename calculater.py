263ef08140d8015b633a8a4a7bec6073dce3043a
Dionshin <bigjoyshin@gmail.com>
2026-03-05 14:54:05 +0900

    나누기 기능 추가

--- calculater.py
+++ calculater.py
@@ -12,12 +12,16 @@ def mul(a,b):
     c= a*b
     return c
 
-
+def div(a,b):
+    c= a/b
+    return c
 
 num1,num2 = map(int,input().split())
 add = add(num1,num2)
 sub = sub(num1,num2)
 mul = mul(num1, num2)
+div = div(num1, num2)
 print(add)
 print(sub)
 print(mul)
+print(div)
